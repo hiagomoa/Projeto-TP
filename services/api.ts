e2ff3f5503124ffa41c0be0b511116d1c85d0f26
hiagomoa <54142434+hiagomoa@users.noreply.github.com>
2020-12-06 21:06:45 -0300

    Update api.ts

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://backendtopicos.herokuapp.com/",
+  baseURL: "http://backpatricia.herokuapp.com",
 });
 
 export default api;
